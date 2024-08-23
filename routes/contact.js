422f38456def5a77f02f62692b0059ad22f8541c
Rocio Grajales <rocioegrajales@gmail.com>
2024-08-22 22:52:31 -0500

    Fix error contact response

--- routes/contact.js
+++ routes/contact.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
-const Contact = require ('../models/Contact');
+const Contact = require('../models/Contact');
 
-router.post('/send', async(req, res) => {
-    const {name, lastname, email, birthday} = req.body;    
-
-    try{
-        const cnew = await Contact.create({
-        name,
-        lastname,
-        email,
-        birthday      
-        })       
-        console.log(cnew)
-    }catch (err){
+router.post('/send', async (req, res) => {
+    const { name, lastname, email, birthday } = req.body;
+    try {
+        const newContact = await Contact.create({
+            name,
+            lastname,
+            email,
+            birthday
+        });
+        res.status(200).json(newContact);
+    } catch (err) {
         console.error(err.message);
-        res.status(500).send('Error al enviar respuetas')
+        res.status(500).json({ error: 'Error al enviar respuesta' });
     }
 });
 
