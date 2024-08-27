a984568db49dcb3ccdc9f50aa3eb14bbe6c7fb21
Rocio Grajales <rocioegrajales@gmail.com>
2024-08-27 16:07:04 -0500

    Eliminar req

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors('https://allusersfront.onrender.com'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
+app.get('/', (res) => {   
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
