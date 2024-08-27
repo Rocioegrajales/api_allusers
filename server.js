e0b016d3321da41e122fe59614de4bbee3ca13e3
Rocio Grajales <rocioegrajales@gmail.com>
2024-08-27 16:13:24 -0500

    Put req again

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const {sequelize} = require('./config/db');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 require ("dotenv").config()
 
 const app = express();
@@ -10,7 +11,7 @@ const app = express();
 app.use(cors('https://allusersfront.onrender.com'));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (res) => {   
+app.get('/', (req, res) => {   
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
